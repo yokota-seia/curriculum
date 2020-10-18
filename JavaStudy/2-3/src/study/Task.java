6fd7248916d669a52800c1a3d412bc2f8dc0c0eb
yokota-seia <70831329+yokota-seia@users.noreply.github.com>
2020-10-18 17:48:32 +0900

    オーバーロード・継承・オーバーライド
    オーバーロード・継承・オーバーライドの課題を提出します

---
+++ JavaStudy/2-3/src/study/Task.java
@@ -0,0 +1,28 @@
+package study;
+
+
+//① TaskクラスにCalculatorクラスを継承させなさい。
+public class Task extends Calculator{
+	/**
+	 * タスクの実行
+	 */
+	public void doTask() {
+
+		// ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
+		// 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
+		int a = 10;
+		int b = 20;
+		int c = 30;
+
+		plus(a);
+		System.out.println("plusメソッドの引数が一つの場合：" + plus(a));
+
+		plus(a,b);
+		System.out.println("plusメソッドの引数が二つの場合：" + plus(a,b));
+
+
+		plus(a,b,c);
+		System.out.println("plusメソッドの引数が三つの場合：" + plus(a,b,c));
+
+	}
+}
